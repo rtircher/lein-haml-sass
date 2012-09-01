122fcca2779f44acde8e0c4bf3e945a138be5385
Renaud Tircher <rtircher@thoughtworks.com>
2012-09-01 09:19:10 -0700

    Cleaning up the haml processing

--- src/leiningen/lein_haml.clj
+++ src/leiningen/lein_haml.clj
@@ -1,22 +1,10 @@
 (ns leiningen.lein-haml
-  (:use clojure.contrib.io)
-  (:use clojure.contrib.classpath))
+  (:import [org.jruby.embed ScriptingContainer LocalContextScope]))
 
-(import '(org.jruby.embed ScriptingContainer LocalContextScope))
-(def c (ScriptingContainer. LocalContextScope/THREADSAFE))
-
-(println (classpath))
-(println (pwd))
-
-;; Using $LOAD_PATH to load haml gem
-;(def gempath [(str (pwd) "/src/haml-3.1.2/gem")])
-;(. c setLoadPaths gempath)
-;(. c runScriptlet "require 'rubygems'; require 'haml'")
-
-;; Using classpath to load haml gem
+(def ^{:private true} c (ScriptingContainer. LocalContextScope/THREADSAFE))
 (. c runScriptlet "require 'rubygems'; require 'haml'")
 
-(def engineclass (. c runScriptlet "Haml::Engine"))
+(def ^{:private true} engineclass (. c runScriptlet "Haml::Engine"))
 
 (defn render [template]
   (let [engine (. c callMethod engineclass "new" template Object)]
