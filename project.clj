d4d524c7a76036b7087fb272c777114599553b9b
Renaud Tircher <rtircher@thoughtworks.com>
2012-08-31 22:22:27 -0700

    Setup initial project structure

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject lein-haml "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.jruby/jruby-complete "1.6.4"]]
+
+  :main leiningen.lein-haml
+
+  :extra-classpath-dirs ["/Users/rtircher/Development/lein-haml/resources" "/Users/rtircher/Development/lein-haml/resources/haml" "gems"]
+  :resource-paths ["/Users/rtircher/Development/lein-haml/resources"]
+
+  )
---
+++ test/lein_haml/test/core.clj
@@ -0,0 +1,6 @@
+(ns lein-haml.test.core
+  (:use [lein-haml.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
