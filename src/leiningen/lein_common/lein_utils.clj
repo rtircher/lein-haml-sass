751ed6479bdec4a6947cf0f04e98e0e429e3fbd3
Renaud Tircher <rtircher@thoughtworks.com>
2012-09-25 08:32:30 -0700

    Extracting utilities for leiningen

---
+++ src/leiningen/lein_common/lein_utils.clj
@@ -0,0 +1,25 @@
+(ns leiningen.lein-common.lein-utils)
+
+(def ^:private lein2?
+  (try
+    (require 'leiningen.core.main)
+    true
+    (catch java.io.FileNotFoundException _
+      false)))
+
+(defn exit-success
+  "Exit successfully in a way that satisifies lein1 and lein2."
+  []
+  (when-not lein2? 0))
+
+(defn exit-failure
+  "Fail in a way that satisifies lein1 and lein2."
+  [error-msg]
+  (if lein2?
+    ((resolve 'leiningen.core.main/abort) error-msg)
+    (do
+      (println error-msg)
+      1)))
+
+(defn task-not-found [subtask]
+  (exit-failure (str "Subtask \"" subtask "\" not found.")))
