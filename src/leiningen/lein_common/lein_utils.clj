52baddd8bab44e3d6da71c10ef5149decc701c30
Renaud Tircher <renaud.tircher@gmail.com>
2012-11-18 23:26:52 -0800

    Checking if the gem is installed before installing it

--- src/leiningen/lein_common/lein_utils.clj
+++ src/leiningen/lein_common/lein_utils.clj
@@ -29,7 +29,15 @@
 (defn install-gem! [{:keys [gem-name gem-version]}]
   (when gem-version
     (let [container (ScriptingContainer. LocalContextScope/THREADSAFE)]
-      (println (str "Installing: " gem-name "-" gem-version))
-      (.runScriptlet container (str "require 'rubygems'
+      (.setLoadPaths container [(str "lib/gems/gems/" gem-name "-" gem-version "/lib")])
+      (.runScriptlet container (str "
+require 'rubygems'
 require 'rubygems/dependency_installer'
-Gem::DependencyInstaller.new(:install_dir => 'lib/gems').install('" gem-name "', '" gem-version "')")))))
+
+begin
+  Gem::Specification.find_by_name('" gem-name "', '" gem-version "')
+rescue Gem::LoadError
+  puts 'Installing: " gem-name "-" gem-version "'
+  Gem::DependencyInstaller.new(:install_dir => 'lib/gems').install('" gem-name "', '" gem-version "')
+end
+")))))
