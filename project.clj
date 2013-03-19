bd9c99a3840d8140bbbeb66bb08d735e7d52c54d
Renaud Tircher <renaud.tircher@gmail.com>
2013-03-19 23:36:22 +0800

    Fix incorrect "doc"

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
   :sass {:src "spec/files"
          :output-directory "spec/out/sass"
          ;; Other options (provided are default values)
-         ;; :output-extension "css"
+         ;; :output-extension css
          ;; :auto-compile-delay 250
          ;; :delete-output-dir true ;; -> when running lein clean it will delete the output directory if it does not contain any file
          :ignore-hooks [:deps] ;; -> if you ue the hooks, allows you to remove some hooks that you don't want to run
@@ -43,7 +43,7 @@
   :scss {:src "spec/files"
          :output-directory "spec/out/scss"
          ;; Other options (provided are default values)
-         ;; :output-extension "css"
+         ;; :output-extension css
          ;; :auto-compile-delay 250
          ;; :delete-output-dir true ;; -> when running lein clean it will delete the output directory if it does not contain any file
          ;; :ignore-hooks [:clean :compile] ;; -> if you ue the hooks, allows you to remove some hooks that you don't want to run
